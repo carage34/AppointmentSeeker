c4b4b71c6beaa12a4979964f9b0c5817d4707d58
mjully <jully.morgan@orange.fr>
2022-03-12 18:25:22 +0100

    Fix promise logic + clean code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { Client, Intents } = require('discord.js');
 const { token, channelId} = require('./config.json');
 const cheerio = require('cheerio');
-const axios = require('axios');
 const request = require('request');
 const moment = require('moment');
+const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -47,35 +47,41 @@ listeRdv['Nangis'] = dataRdvNangis;
 
 client.once('ready', c => {
     console.log("Ready");
+    const listPromise = []
     client.channels.cache.get(channelId).send("---------------------------");
-    parsePage(client, "Provins", listeRdv["Provins"]).then(() => {
-        parsePage(client, "Nangis", listeRdv["Nangis"]).then(() => {
-            client.channels.cache.get(channelId).send("---------------------------").then(() => {
-                process.exit()
-            })
-
-        })
+    let file_content = fs.readFileSync('date-rdv.json');
+    let content = JSON.parse(file_content);
+    for(key in listeRdv) {
+        listPromise.push(parsePage(client, key, listeRdv[key], content));
+    }
+    Promise.all(listPromise).then((values) => {
+        if(values[0] === false && values[1] === false) {
+            client.channels.cache.get(channelId).send("Batch executé : Pas de nouveaux créneaux libérés");
+        }
+        client.channels.cache.get(channelId).send("---------------------------").then(() => {
+            process.exit()
+        });
     })
 });
 
 client.login(token);
 
-function parsePage(client, ville, data) {
+function parsePage(client, ville, data, content) {
      return new Promise((resolve, reject) => {
+
+         let newRdv = false;
+
          request.post(
              {
                  url: data.url,
                  form: data.body
              },
              function (err, httpResponse, body) {
-
                  const $ = cheerio.load(body);
                  const dates = [];
                  let bold = $('.bold');
                  if($(bold).html()) {
-
-                     client.channels.cache.get(channelId).send("Pas de créneaux disponible");
-                     resolve();
+                     resolve(false);
                  }
                  $('a').each((index, el) => {
 
@@ -85,14 +91,22 @@ function parsePage(client, ville, data) {
                          value.lastIndexOf(")")
                      )
                      if(!splitP.includes((','))) {
-                         const date = moment(splitP.split('\'')[1].split(' ')[0]).format('DD/MM/YYYY');
+                         let date = moment(splitP.split('\'')[1].split(' ')[0]).format('DD/MM/YYYY');
                          if(!dates.includes(date)) {
+                             let splitted = date.split('/');
+                             let fileSplitted = content[ville].split('/');
+                             if(new Date(parseInt(splitted[0], 10), parseInt(splitted[1], 10), parseInt(splitted[2], 10)) < new Date(parseInt(fileSplitted[0], 10), parseInt(fileSplitted[1], 10), parseInt(fileSplitted[2], 10))) {
+                                 newRdv = true;
+                                 content[ville] = splitted[0] + '/' +  splitted[1] + '/' +  splitted[2];
+                                 fs.writeFileSync('date-rdv.json', JSON.stringify(content));
+                                 client.channels.cache.get(channelId).send("Nouveau créneau disponible à " + ville  + " le " + date);
+                             }
                              dates.push(date);
-                             client.channels.cache.get(channelId).send("Créneau disponible à " + ville  + " le " + date);
+
                          }
                      }
                  });
-                 resolve();
+                 resolve(newRdv);
              }
          );
      })
